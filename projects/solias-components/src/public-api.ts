ba759f52c8a7b66433e45693deeb0f95c823751e
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-03-11 08:44:17 +0530

    Add select directive

--- projects/solias-components/src/public-api.ts
+++ projects/solias-components/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/radio/radio.directive';
 export * from './lib/input/solias-input.directive';
 export * from './lib/button/solias-button.directive';
 export * from './lib/textarea/textarea.directive';
+export * from './lib/select/select.directive';
